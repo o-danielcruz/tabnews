3ae601c15d0cfce04c45172336e327bc25f03230
Daniel Cruz <danielcruz906@gmail.com>
2026-02-05 02:21:28 +0000

    Adicionando novo commit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,33 @@
 
-function Home(){
-    return <h1> Babe, eu te amo! Se tu me ama também, dá uma risadinha. </h1>
+function Home() {
+    return (
+        <>
+            <h1>Babe, eu te amo! Se tu me ama também, dá uma risadinha.</h1>
+            <br />
+            <h2>Fique com este poema:</h2>
+            <br />
+            <p>
+                <i>
+                    Minha vidoca, vidoquinha
+                    <br />
+                    Já não sei o que fazer
+                    <br />
+                    Minha vida se resume
+                    <br />
+                    A só pensar em você
+                    <br />
+                    <br />
+                    Seja andando pela rua
+                    <br />
+                    Seja sentado na cadeira
+                    <br />
+                    Eu só penso na vidoca
+                    <br />
+                    Que eu amo por inteira
+                </i>
+            </p>
+        </>
+    );
 }
 
 export default Home;
